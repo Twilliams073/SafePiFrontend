3a7e7cd5ecd3d04e1e891fa5693516449f25d674
Twilliams073 <tristanwilliamsrc@gmail.com>
2023-03-13 13:32:30 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ListSensorComponent from './components/ListSensorComponent';
 
 function App() {
   return (
     <div>
-      <ListSensorComponent/>
+      <Router>
+        {/* Header */}
+        <div className= "container" >
+          <Switch>
+            <Route exact path = "/" component = {ListSensorComponent}></Route>
+            <Route path = "/sensors" component = {ListSensorComponent}></Route>
+          </Switch>
+          <ListSensorComponent/>
+        </div>
+        
+        {/* Footer */}
+      </Router>
+      
     </div>
   );
 }
